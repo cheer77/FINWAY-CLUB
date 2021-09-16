d8705f016420efd7c2b938eadb7951fb649b5aeb
Aleksandr <sheph@ukr.net>
2021-09-16 22:36:57 +0300

    added half part of board page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
 
+    $(".js-range-slider").ionRangeSlider({
+        grid: true,
+    });
 
- 
+    let range = document.querySelector('.board__progress-num');
+    let rangeNum = document.querySelector('.board__progress-text');
+    range.oninput = function () {
+        console.log(this.value);
+        rangeNum.innerHTML = this.value;
+    };
 
 });
